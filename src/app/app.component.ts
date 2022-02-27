881fd241bc45b6e9b9eecf2963d6aed51ba951cb
AnetaSzolikova <anetaszolikova6@gmail.com>
2022-02-27 19:28:00 +0100

    book html + ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,12 @@ interface User{
   name: string;
   contact: string;
 }
+interface Book{
+  id: number;
+  name: string;
+  author: string;
+  available: boolean;
+}
 interface Borrowing{
   id: number;
   book: string;
@@ -28,6 +34,13 @@ export class AppComponent {
     name: '',
     contact: ''
   }
+  books : Book[] = [];
+  book: Book = {
+    id: 0,
+    name: '',
+    author: '',
+    available: true
+  }
   borrowings : Borrowing[] = [];
   borrowing : Borrowing = {
     id: 0,
@@ -40,6 +53,10 @@ export class AppComponent {
   addUser(){
     let u2 = {id: this.user.id, name: this.user.name, contact: this.user.contact}
     this.users.push(u2);
+  }
+  addBook(){
+    let b2 = {id: this.book.id, name: this.book.name, author: this.book.author, available: this.book.available}
+    this.books.push(b2);
   }
   addBorrowing(){
     let bo = {id: this.borrowing.id, book: this.borrowing.book, user: this.borrowing.user}
