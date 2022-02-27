387d480bfa1fe3b5812912266f8e21e1be8cc2c5
MichalToth5 <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>
2022-02-27 18:53:35 +0100

    pridanie user .html .ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,11 @@ enum MENU{
   BOOKS,
   BORROWINGS
 }
+interface User{
+  id: number;
+  name: string;
+  contact: string;
+}
 interface Borrowing{
   id: number;
   book: string;
@@ -17,6 +22,12 @@ interface Borrowing{
 export class AppComponent {
   title = 'BookLibrary';
   activeMenu: MENU = MENU.USER;
+  users : User[] = [];
+  user : User = {
+    id: 0,
+    name: '',
+    contact: ''
+  }
   borrowings : Borrowing[] = [];
   borrowing : Borrowing = {
     id: 0,
@@ -25,6 +36,10 @@ export class AppComponent {
   }
   openWindow(m: MENU) {
     this.activeMenu = m;
+  }
+  addUser(){
+    let u2 = {id: this.user.id, name: this.user.name, contact: this.user.contact}
+    this.users.push(u2);
   }
   addBorrowing(){
     let bo = {id: this.borrowing.id, book: this.borrowing.book, user: this.borrowing.user}
