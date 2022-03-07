b4eac47cd640bd5f89691a01b8ba26c58797f65c
MichalToth5 <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>
2022-03-07 18:10:10 +0100

    upravenie route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BorrowingPageComponent} from "./borrowing-page/borrowing-page.component";
+import {UserStrankaComponent} from "./user-stranka/user-stranka.component";
 
 
 const routes: Routes = [
@@ -8,6 +9,10 @@ const routes: Routes = [
     path: "borrowing",
     component: BorrowingPageComponent
   },
+  {
+    path:"user",
+    component: UserStrankaComponent
+  }
 ];
 
 @NgModule({
